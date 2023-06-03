e2d7204cee485ab244e9f548b9c4626a37e151bb
RazvanApt <apatean.razvan@gmail.com>
2023-06-03 22:19:45 +0200

    Insert CSS data for zero-shot training

---
+++ create_files_css_dataset.py
@@ -0,0 +1,45 @@
+"""
+
+get the cap.{split}.json files from css/json (split = ['train', 'val', 'test'])
+combine the files split.{split}.json into one file
+
+name the new files for 'dress' type of cloths
+
+"""
+import json
+import os
+
+css_path = "D:\Razvan\Documents\CURSURI UAB\Dissertation\Pic2Word\data\css"
+is_path = os.path.join(css_path, "image_splits")
+json_path = os.path.join(css_path, "json")
+
+output_is_filename = "new_split.dress.val.json"
+output_j_filename = "new_cap.dress.val.json"
+
+
+total_is_arr = []
+total_j_arr = []
+
+for split in ['train', 'val', 'test']:
+    with open(json_path + '\\' + f"cap.{split}.json", "r") as caption_file:
+        temp_arr = json.load(caption_file)
+    total_j_arr += temp_arr
+
+with open(json_path + '\\' + output_j_filename, "w") as new_caption_file:
+    json.dump(total_j_arr, new_caption_file)
+
+used_images = []
+for item in total_j_arr:
+    if ('target' in item and item['target'] not in used_images):
+        used_images.append(item['target'])
+
+    if ('candidate' in item and item['candidate'] not in used_images):
+        used_images.append(item['candidate'])
+
+with open (is_path + '\\' + output_is_filename, "w") as new_image_split_file:
+    json.dump(used_images, new_image_split_file)
+
+
+print("DONE")
+
+
