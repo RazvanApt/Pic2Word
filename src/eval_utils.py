bda5feebcc1fce7b9d8b288bf894703fce239de4
RazvanApt <apatean.razvan@gmail.com>
2023-08-03 19:18:24 +0200

    Start to add functionality for css

--- src/eval_utils.py
+++ src/eval_utils.py
@@ -870,11 +870,12 @@ def get_metrics_imgnet(query_features, image_features, query_labels, target_labe
 def get_metrics_css(image_features, ref_features, target_names, answer_names, all_reference_names, all_captions, feature):
     metrics = {}
     distances = 1 - ref_features @ image_features.T    
-    torch.cuda.empty_cache()
+    # torch.cuda.empty_cache()
     logging.info(f"Metrics - Before Argsort. Distance variable device: {distances.device}")
     sorted_indices = torch.argsort(distances, dim=-1).cpu()
     logging.info("Metrics - after Argsort")
-
+    logging.info(f'Sorted_indexes length: {sorted_indices.shape}')
+    logging.info(f'Target_names length: {len(target_names)}')
     sorted_index_names = np.array(target_names)[sorted_indices]
     labels = torch.tensor(
         sorted_index_names == np.repeat(np.array(answer_names), len(target_names)).reshape(len(answer_names), -1))
