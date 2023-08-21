27d5d5e01b1bfa07f763adabe39cc940250205c7
Razvan Apatean <razvanapatean@192-168-0-181.rdsnet.ro>
2023-08-22 01:39:43 +0300

    Add CSS functionality

--- src/eval_utils.py
+++ src/eval_utils.py
@@ -758,9 +758,9 @@ def evaluate_css(model, img2text, args, source_loader, target_loader, preprocess
             if args.gpu is not None:
                 target_images = target_images.cuda(args.gpu, non_blocking=True)
             # logging.info(f"Target Paths: {target_paths}")
-            # image_features = m.encode_image(target_images)
-            image_features, max_nr_objs = computeImageFeaturesOfBatch(m, target_images, target_paths, preprocess_val, args)
-            image_features = image_features.cuda()
+            image_features = m.encode_image(target_images)
+            # image_features, max_nr_objs = computeImageFeaturesOfBatch(m, target_images, target_paths, preprocess_val, args)
+            # image_features = image_features.cuda()
             
             # logging.info(f"Image features: shape {image_features.shape}; type {type(image_features)}; device {image_features.device}; max_nr_obj: {max_nr_objs}")
             # logging.info(f"Image features [0]: shape {image_features[0].shape}; type {type(image_features[0])}")
@@ -787,12 +787,12 @@ def evaluate_css(model, img2text, args, source_loader, target_loader, preprocess
                 caption_only = caption_only.cuda(args.gpu, non_blocking=True)
             # logging.info(f"Reference Names: {ref_names}")
             
-            # image_features = m.encode_image(target_images)
-            # query_image_features = m.encode_image(ref_images)
-            image_features, _ = computeImageFeaturesOfBatch(m, ref_images, answer_paths, preprocess_val, args)
-            image_features = image_features.cuda()
-            query_image_features, max_nr_objs = computeImageFeaturesOfBatch(m, ref_images, ref_names, preprocess_val, args)
-            query_image_features = query_image_features.cuda()
+            image_features = m.encode_image(target_images)
+            query_image_features = m.encode_image(ref_images)
+            # image_features, _ = computeImageFeaturesOfBatch(m, ref_images, answer_paths, preprocess_val, args)
+            # image_features = image_features.cuda()
+            # query_image_features, max_nr_objs = computeImageFeaturesOfBatch(m, ref_images, ref_names, preprocess_val, args)
+            # query_image_features = query_image_features.cuda()
 
             # logging.info(f"Image features: shape {image_features.shape}; type {type(image_features)}; device {image_features.device}")
             # logging.info(f"Image features [0]: shape {image_features[0].shape}; type {type(image_features[0])}")
