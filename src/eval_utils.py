57c9b3f1e6fe056d48c38bbb7be63638e799e5f8
Razvan Apatean <razvanapatean@192-168-0-181.rdsnet.ro>
2023-08-27 23:45:08 +0300

    Add CSS functionality

--- src/eval_utils.py
+++ src/eval_utils.py
@@ -292,10 +292,10 @@ def evaluate_css(model, img2text, args, source_loader, target_loader, preprocess
                 if(idx == 10):
                     break
                 idx = idx + 1
-                print(f"\tToken_texts: shape {token_texts.shape}; type {type(token_texts)}")
-                
-
                 token_texts = tokenize(text_with_blanks)[0]
+                print(f"\tToken_texts: shape {token_texts.shape}; type {type(token_texts)}")
+
+                # token_texts = tokenize(text_with_blanks)[0]
                 target_caption_list.append(token_texts)
 
             id_split = tokenize(["*"])[0][1]
