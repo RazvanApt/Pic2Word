3cc0e5300f669bdfcd038732c017e94649777830
Razvan Apatean <razvanapatean@192-168-0-181.rdsnet.ro>
2023-08-27 23:43:43 +0300

    Add CSS functionality

--- src/eval_utils.py
+++ src/eval_utils.py
@@ -285,7 +285,7 @@ def evaluate_css(model, img2text, args, source_loader, target_loader, preprocess
                     else: 
                         blanks += " and *"
                 text_with_blanks += blanks + f", {captions[idx]} and {captions[idx]}" # to be similar to what was in FashionIQ, in terms of captions
-                """
+                
                 print(f"{idx}:\n\tlength = {len(imageObjectsFeatures)}")
                 print(f"\titem[0].length = {len(imageObjectsFeatures[0])}; type: {type(imageObjectsFeatures[0])}; shape: {imageObjectsFeatures[0].shape}")
                 print(f"\tText with blanks: {text_with_blanks}")
@@ -293,7 +293,7 @@ def evaluate_css(model, img2text, args, source_loader, target_loader, preprocess
                     break
                 idx = idx + 1
                 print(f"\tToken_texts: shape {token_texts.shape}; type {type(token_texts)}")
-                """
+                
 
                 token_texts = tokenize(text_with_blanks)[0]
                 target_caption_list.append(token_texts)
